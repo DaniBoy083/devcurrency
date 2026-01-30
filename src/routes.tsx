08559c98cbd6ee9704766273078a026f7169bf30
Daniel Costa Carvalho Martins <danielcostacarvalhomartins06@gmail.com>
2026-01-29 23:54:07 -0300

    header

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/home";
 import { DetailPage } from "./pages/detail";
 import { NotFoundPage } from "./pages/notfound";
+import { Layout } from "./components/layout";
 
 const router = createBrowserRouter([
     {
+        element: <Layout />,
         children: [
             {
                 path: "/",
